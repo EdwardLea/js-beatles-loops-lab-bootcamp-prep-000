33f5e7b004cfa579177c936f8c87262ac1cd6c4f
leaedward87 <leaedward87@gmail.com>
2018-10-30 15:04:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,29 +1,2 @@
 // add solution here
-function theBeatlesPlay (musicians,instrument){
-  var newArray = [];
-  
-  for (var i = 0; i < musicians.length; i++) {
-    newArray.push(musicians[i] +' plays ' + instrument[i]);
-  }
-  
-  return newArray;
-}
 
-function johnLennonFacts (facts){
-  var newArray2 = [];
-  var i =0;
-  while(i<facts.length){
-    newArray2.push(facts[i] +'!!!');
-  }
-}
-
-function iLoveTheBeatles(x){
-  var newArray3 = [];
-  var i = x;
-function incrementVariable() {
-  i = i + 1;}
-  do {
-    newArray3.push('I love the Beatles!');
-    incrementVariable();
-  }while(i <15);
-}
